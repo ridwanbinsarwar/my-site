2a1364af755f41c0279f4a7af994691ee9d18c89
xenon <cj.rdwn@gmail.com>
2019-02-18 20:13:29 +0600

    "home edited v1.0"

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from home import views as home_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_views.index),
 ]
